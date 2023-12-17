22b599766abbeb04b626d9b10236273e419837c0
Ianuş <103447553+ianusghiletchi@users.noreply.github.com>
2023-12-17 15:37:42 +0200

    Update 0.2*

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,24 +6,49 @@ import axios from 'axios';
 
 function App() {
   const [temperature, setTemperature] = useState(0);
+  const [realFeelTemperature, setRealFeelTemperature] = useState(0);
 
+  // Function to convert Fahrenheit to Celsius
+  function convertFahrenheitToCelsius(fahrenheit) {
+    return Math.round((fahrenheit - 32) * 5 / 9);
+  }
+
+  // Function to handle data refresh
   const handleRefresh = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/api/weather");
-      const todayTemperature = response.data.DailyForecasts[0].Temperature.Minimum.Value;
-      setTemperature(todayTemperature);
+      // Check if cached data exists in local storage
+      const cachedData = localStorage.getItem("weatherData");
+      if (cachedData) {
+        const parsedData = JSON.parse(cachedData);
+        setTemperature(parsedData.temperature);
+        setRealFeelTemperature(parsedData.realFeelTemperature);
+      } else {
+        // Fetch data from the server if no cached data
+        const response = await axios.get("http://localhost:5000/api/weather");
+        const todayTemperature = convertFahrenheitToCelsius(response.data.DailyForecasts[0].Temperature.Maximum.Value);
+        const todayRealFeelTemperature = convertFahrenheitToCelsius(response.data.DailyForecasts[0].RealFeelTemperature.Maximum.Value);
+
+        // Update state
+        setTemperature(todayTemperature);
+        setRealFeelTemperature(todayRealFeelTemperature);
+
+        // Cache data in local storage
+        localStorage.setItem("weatherData", JSON.stringify({ temperature: todayTemperature, realFeelTemperature: todayRealFeelTemperature }));
+      }
     } catch (error) {
       console.error(error);
     }
   };
 
+  // Use effect to run on component mount
   useEffect(() => {
     handleRefresh();
   }, []);
+
   return (
     <div>
       <Header onRefresh={handleRefresh} />
-      <Body temperature={temperature} />
+      <Body temperature={temperature} realFeelTemperature={realFeelTemperature} />
       <Footer />
     </div>
   );
--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Body({ temperature }) {
+function Body({ temperature, realFeelTemperature }) {
   return (
     <div>
       <div className="cwi-div">
@@ -8,21 +8,21 @@ function Body({ temperature }) {
       </div>
       <div className="temp-div">
         <h1 style={{ fontSize: '5rem', color: 'aliceblue', justifySelf: 'center', marginTop: '30px', marginBottom: '20px' }}>
-          {temperature}°C
+          {temperature}°
         </h1>
-        <h3 style={{ fontSize: '2rem', color: 'aliceblue', margin: '0%' }}>Moldova</h3>
+        <h3 style={{ fontSize: '2rem', color: 'aliceblue', margin: '0%' }}>RealFeel {realFeelTemperature}°</h3>
       </div>
       <div className="future-wcast">
         <div className="fwc-day">
-          <h3>Tuesday</h3>
+          <i class="bi bi-wind"></i>
           <h3>-2</h3>
         </div>
         <div className="fwc-day">
-          <h3>Wednesday</h3>
+          <i class="bi bi-lungs"></i>
           <h3>2</h3>
         </div>
         <div className="fwc-day">
-          <h3>Thursday</h3>
+          <i class="bi bi-umbrella"></i>
           <h3>3</h3>
         </div>
       </div>
